3590b3be5e33e3ff53c248f8b9a7f1e3361c5cc6
Marjorie <marjoriebaut.w@gmail.com>
2023-05-15 01:32:54 -0400

    browser res

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const routes = require('./controllers');
 const PORT = 3005;
 const app = express();
+
+app.use(routes);
 
 app.listen(PORT,()=> {
     console.log('work')
